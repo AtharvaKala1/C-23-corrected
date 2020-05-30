b63553762c4ef307041ef549e3561be1c00aad3e
AtharvaKala1 <65211079+AtharvaKala1@users.noreply.github.com>
2020-05-30 09:16:49 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,74 @@
+var helicopterIMG, helicopterSprite, packageSprite,packageIMG;
+var packageBody,ground;
+const Engine = Matter.Engine;
+const World = Matter.World;
+const Bodies = Matter.Bodies;
+const Body = Matter.Body;
+const Render = Matter.Render;
+function preload()
+{
+	helicopterIMG=loadImage("helicopter.png")
+	packageIMG=loadImage("package.png")
+}
+
+function setup() {
+	createCanvas(800, 700);
+	rectMode(CENTER);
+
+	packageSprite=createSprite(width/2, 200, 10,10);
+	packageSprite.addImage(packageIMG)
+	packageSprite.scale=0.2
+
+	helicopterSprite=createSprite(width/2, 200, 10,10);
+	helicopterSprite.addImage(helicopterIMG)
+	helicopterSprite.scale=0.6
+
+	groundSprite=createSprite(width/2, height-35, width,10);
+	groundSprite.shapeColor=color(255)
+
+	//creating the engine
+	engine = Engine.create();
+	world = engine.world;
+	
+	//creating the bodies
+	packageBody = Bodies.rectangle(packageSprite.x, packageSprite.y , 50 , 50, {restitution:0.8,isStatic:true,mass:50});
+	World.add(world, packageBody);
+	
+
+	//Create a Ground
+	ground = Bodies.rectangle(groundSprite.position.x, groundSprite.position.y , width, 10 , {isStatic:true} );
+	 World.add(world, ground);
+
+	 //creating the box
+	 box1 = new Box(width/2-100,height-90,20,100);
+	 box2 = new Box(width/2+10,height-50,200,20);
+	 box3 = new Box(width/2+120,height-90,20,100);
+
+	 //running the engine
+	 Engine.run(engine);
+  
+}
+
+
+function draw() {
+  rectMode(CENTER);
+  background(0);
+  //setting the sprite position to the body position
+  packageSprite.x= packageBody.position.x 
+  packageSprite.y= packageBody.position.y 
+  drawSprites();
+  box1.display();
+  box2.display();
+  box3.display();
+ 
+}
+
+function keyPressed() {
+ if (keyCode === DOWN_ARROW) {
+    Matter.Body.setStatic(packageBody,false);
+    
+  }
+}
+
+
+
